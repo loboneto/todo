7bfe03b5fd2fc6a8284a6445f9f9e9374652ead9
suporte18@rcostaconsult.com.br <lobonetoc@gmail.com>
2021-11-04 12:24:22 -0300

    telas de task e subtask

--- app/src/main/java/br/com/ufersa/bd/todo/presentation/TasksActivity.kt
+++ app/src/main/java/br/com/ufersa/bd/todo/presentation/TasksActivity.kt
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.ufersa.bd.todo.R
 import br.com.ufersa.bd.todo.databinding.ActivityMainBinding
+import br.com.ufersa.bd.todo.databinding.ActivityTasksBinding
 import br.com.ufersa.bd.todo.domain.utils.viewBindings
 
-class MainActivity : AppCompatActivity() {
+class TasksActivity : AppCompatActivity() {
 
-    private val binding by viewBindings(ActivityMainBinding::inflate)
+    private val binding by viewBindings(ActivityTasksBinding::inflate)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
