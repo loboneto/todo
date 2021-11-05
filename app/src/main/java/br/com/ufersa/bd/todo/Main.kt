c860a4e9ff3abf2d400bcf3e2fd54afb87fd5ce4
suporte18@rcostaconsult.com.br <lobonetoc@gmail.com>
2021-11-05 10:46:05 -0300

    ajuste no hilt

---
+++ app/src/main/java/br/com/ufersa/bd/todo/Main.kt
@@ -0,0 +1,9 @@
+package br.com.ufersa.bd.todo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Main: Application() {
+    
+}
--- app/src/main/java/br/com/ufersa/bd/todo/di/RoomModule.kt
+++ app/src/main/java/br/com/ufersa/bd/todo/di/RoomModule.kt
@@ -6,12 +6,12 @@ import br.com.ufersa.bd.todo.domain.local.TasksDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object RoomModule {
 
     @Singleton
